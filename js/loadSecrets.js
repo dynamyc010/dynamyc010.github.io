7ab432347eeb605bb04ffe4f2237ed397edfd718
dynamyc | Bálint Sebestyén <33225256+dynamyc010@users.noreply.github.com>
2020-10-11 13:18:43 +0200

    Update terminal and stuff idk lol

---
+++ js/loadSecrets.js
@@ -0,0 +1,63 @@
+// Loads the Super Secret Terminal!
+let isTerminalOpen = 0; 
+function weLostOurSanity()
+{
+    if(isTerminalOpen == 0){
+        var ifreem = document.createElement("IFRAME");
+        ifreem.setAttribute('class', 'consolas'); 
+        ifreem.setAttribute('src', "/terminal/");
+        
+        document.body.appendChild(ifreem);
+
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    }
+    else{
+        switch(isTerminalOpen){
+            case 1:
+                alert("Don't try and make more terminals, please.");
+                break;
+            case 2:
+                alert("Don't try and make more terminals, please.");
+                break;
+            case 3:
+                alert("Seriously, don't.");
+                break;
+            case 4:
+                alert("Are you doing this to anger me?");
+                break;
+            case 5:
+                alert("Orr... maybe you don't know english well? \n\nWell, I can't do much about it.");
+                break;
+            case 6:
+                alert("Or maybe you just want a shinier terminal? I hope not.");
+                break;
+            case 7:
+                alert("Hmm.");
+                break;
+            case 8:
+                alert("I'mma just... gonna go get a drink. You keep doing whatcha doing.")
+                break;
+            case 12:
+                alert("Oh, you're still here.");
+                alert("Why are you doing this?");
+                alert("At least tell me THAT.");
+                break;
+            case 13:
+                alert("You know what? YOU WIN! You'll get your second terminal! Just... please don't press that button again.")
+                var ifreem = document.createElement("IFRAME");
+                ifreem.setAttribute('class', 'consolas'); 
+                ifreem.setAttribute('src', "/terminal/");
+                
+                document.body.appendChild(ifreem);
+                $('html, body').animate({ scrollTop: 0 }, 'fast');
+                setTimeout(() => {alert("There. Happy? \n\nYou will only be able to see a single terminal but you can inspect element to see you indeed have 2!"); }, 600);
+                break;
+            case 14:
+                alert("I'm fucking done with you.")
+                open(location, '_self').close();
+                break;
+
+        }
+    }
+    isTerminalOpen++; // Terminal should now be open. Let's not make more of them.
+}
--- terminal/cmds.js
+++ terminal/cmds.js
@@ -5,28 +5,71 @@
 // for hyperlinks, use [[!;;;;linkhere]text], same formatting is used for the original, just the extra ; is for links i believe
 // for images. use [[@;;;;linkhere]]
 
+// God help me.
+
+let ip = "0.0.0.0";
+let note = "\n";
+let ipcolor = "#22b352"
+
+$(document).ready(function () {
+    $.getJSON("https://geo.ipify.org/api/v1?apiKey=at_Dxpfkozoqs8jWbKBjvdJP7UIsBaCO", function (data) {
+        ip = data.ip;
+    });
+    setTimeout(function(){
+    if(ip == "0.0.0.0"){
+        ip = "unknown"
+        note = "\n\n[[;#FF0000;;](!!!)] Your IP is currently unavailable. More info in devtools."
+        console.log("An ad-blocker (probably) blocked our request to ipify, or ipify just timed out lol. \nYou probably won't miss out on much.")
+        ipcolor = "#FF0000"
+    }}, 700);
+});
+
+setTimeout(function(){
 $('body').terminal({
     testcommand: function() {
         this.echo("owo");
     },
+    wiki: function(command){
+        if(command != null){
+            this.echo(command);
+        }
+    },
     help: function() {
         this.echo("This terminal is still very much a work-in-progress, but! Some commands are already available! \n\n"
-        + "Current commands available: "
+        + "Current commands available: \n"
         // Start of commands
-        + "testcommand - Prints out owo as of right now."
+        + "[[;#22b352;;]testcommand] - Prints out owo as of right now. \n"
+        + "[[;#22b352;;]about] - Shows information about the OS."
         // End of commands
         + "\n\n\n"
         + "Thank you for helping us help you help us all."
         )
-    }
+    },
+    about: function(){
+
+    this.echo(
+      " _____ _       _ _ _         _   _ _____         _   _         \n" 
+    + "|_   _| |_ ___| | | |___ ___| |_| |     |___ ___| |_|_|___ ___ \n"
+    + "  | | |   | -_| | | | . |  _| | . | | | | .'|  _|   | |   | -_|\n"
+    + "  |_| |_|_|___|_____|___|_| |_|___|_|_|_|__,|___|_|_|_|_|_|___|\n"
+    + "                                                               \n"
+    + "[[@;;;;./assets/PC_on.png]] \n\n"
+    + "Version: 2 \n"
+    + "Current session: [[;#22b352;;]guest]@[[;" + ipcolor + ";;]" + ip + "]"
+
+    
+    )}
 },
 {
-    greetings: "Last login: [[;#FF0000;;]unknown]" + "\n"
-    + "Welcome to The World Machine!" + "\n\n"
-    + "Message by [[;#00FF00;;]worldmachine@127.0.0.1] on Sat Oct  10 21:36:07 GMT+1 2020:" + "\n"
-    + '[[;#0f0fa6;;]"Welcome to this super-cozy terminal!' + '\n'
-    + "Cool, right? You can get started by typing 'help'!" + '"]' + "\n",
+    greetings:
+      " _____ _       _ _ _         _   _ _____         _   _         \n" 
+    + "|_   _| |_ ___| | | |___ ___| |_| |     |___ ___| |_|_|___ ___ \n"
+    + "  | | |   | -_| | | | . |  _| | . | | | | .'|  _|   | |   | -_|\n"
+    + "  |_| |_|_|___|_____|___|_| |_|___|_|_|_|__,|___|_|_|_|_|_|___|\n"
+    + "                                                               \n"
+    + "[[@;;;;./assets/PC_on.png]]"
+    + "Last login: [[;#FF0000;;]unknown] from [[;"+ ipcolor +";;]" + ip + "]\n"
+    + "Welcome to The World Machine!" + note + "\n",
+    prompt: "guest@dynamyc.me:~$ "
     
-    prompt: "guest@dynamyc.me:~$"
-}
-);
+});}, 1200);
