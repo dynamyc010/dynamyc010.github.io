70f926d828a759616661c3401c8cbfe59cfc96c4
dynamyc | Bálint Sebestyén <33225256+dynamyc010@users.noreply.github.com>
2020-10-13 16:59:13 +0200

    Added site control

--- js/redirect-directaccesstosite.js
+++ js/redirect-directaccesstosite.js
@@ -1,4 +1,11 @@
+// This file is used in order to redirect users who try to direct-access sites I don't want them to. (Like the 404 page)
+
 function redirectIfDirect(){
-// Hi
-console.log(window.location.href);
+if(window.location.href == "https://dynamyc.me/404.html")
+    {
+        window.location.href = "https://dynamyc.me/cheatersjail.html"
+    }
+else{
+    console.log("The website you tried to access (" + window.location.href + ") does not exist. If you have questions about this, contact me.")
+    }
 }
