45dd6f6be1923dd44097b78328f41bb40c6cdd65
dynamyc <balint.sebestyen2004@gmail.com>
2024-01-28 15:23:30 +0100

    Update Titles

--- js/changetitle.js
+++ js/changetitle.js
@@ -1,38 +1,39 @@
 const weblocation = window.location.hostname;
 
-const titleJSON = `{
+const raw = `{
     "titles" : 
     [ 
+        "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
         "Welcome to dyn's very own web location!",
         "How are ya?",
         "fukc",
         "icasnthsothavingstorkes", 
         "owo uwu owo",
         "meep",
-        "I may have helped a little in G4, I dunno lol",
         "Boo!",
         "Did you know that this is random every time?",
-        "Nox mode was a joke. Probably.",
         "If you see this, it works!",
-        "Follow me on Tweeter lol",
         "Who dares enter my dungeon?",
         "hewwo owo",
         "brb, yeeting myself out of existence",
         "meow",
         "how do you do fellow kids",
         "what is life",
-        "The Council of Niko is a nice place, not gonna lie.",
         "red kinda sus ngl",
         ">w<",
         "hehe",
-        "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+        "a great great asset to the company",
+        "mikor lesz már ncs?",
+        "beeping intensifies (beep beep)",
+        "I'm a little teapot, short and stout",
+        "happy 2008!!!"
     ]
 }`
-const onloadStrings = JSON.parse(titleJSON).titles;
+
+const onloadStrings = JSON.parse(raw).titles;
 
 function changeTitle()
 {
     let genNumber = Math.floor(Math.random()*onloadStrings.length);
     document.title = weblocation + " | " + onloadStrings[genNumber];
-
 }
